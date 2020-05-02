a4df7fa963a7d57726ca8324fc9a921240f9035f
annoor98 <annoor98@hotmail.com>
2020-05-01 19:12:13 -0600

    Added the ability to change subreddits.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import axios from 'axios';
 
 class App extends Component{
 
+  /*Right now r/cute is the default subreddit
+    The after variable holds the id for the next set of content to load
+  */
   state = {
+    subreddit: "cute",
     imageList: [
 
     ],
@@ -16,12 +20,18 @@ class App extends Component{
   constructor(props){
     super(props);
     this.didScroll = this.didScroll.bind(this);
+    this.changeSub = this.changeSub.bind(this);
+    this.loadSubs = this.loadSubs.bind(this);
+  }
+
+  //Initial Loading of the subreddit content
+  loadSubs(){
+    axios.get('https://www.reddit.com/r/' + this.state.subreddit + '/.json')
+      .then(response => this.setState({imageList: response.data.data.children, after: response.data.data.after}));
   }
 
   componentDidMount(){
-    axios.get('https://www.reddit.com/r/cute/.json')
-      .then(response => this.setState({imageList: response.data.data.children, after: response.data.data.after}));
-
+    this.loadSubs();
     window.addEventListener('scroll', this.didScroll);
   };
 
@@ -29,24 +39,36 @@ class App extends Component{
     window.removeEventListener('scroll', this.didScroll);
   };
 
+  changeSub(){
+    var subName = document.getElementById("subName").value
+    this.setState({
+      subreddit: subName,
+      imageList: []
+    },
+    this.loadSubs
+    );
+  }
+
+  /*Event that checks how far the user scrolled through the page.
+    When roughly 75% of the screen height is reached, mroe content will get loaded.
+  */
   didScroll = (e) =>{
 
-    console.log(this.state.isAtBottom);
-    console.log(document.getElementById("View").offsetHeight /2 + "      " + document.documentElement.scrollTop);
+    console.log(document.getElementById("View").offsetHeight * 0.75 + "      " + document.documentElement.scrollTop);
 
-    if(document.getElementById("View").offsetHeight /2 <= document.documentElement.scrollTop && this.state.isAtBottom === false){
-      console.log("BOT");
+    if(document.getElementById("View").offsetHeight * 0.75 <= document.documentElement.scrollTop && this.state.isAtBottom === false){
 
+      console.log("BOTROGTAG");
       this.setState({isAtBottom: true});
 
-      axios.get('https://www.reddit.com/r/cute/.json?after=' + this.state.after)
+      axios.get('https://www.reddit.com/r/' + this.state.subreddit + '/.json?after=' + this.state.after)
       .then(response => this.setState({
             imageList: this.state.imageList.concat(response.data.data.children),
             after: response.data.data.after
           }));
     }
 
-    if(document.getElementById("View").offsetHeight/2 > document.documentElement.scrollTop && this.state.isAtBottom === true){
+    if(document.getElementById("View").offsetHeight * 0.75 > document.documentElement.scrollTop && this.state.isAtBottom === true){
       this.setState({isAtBottom: false});
     }
   };
@@ -54,6 +76,11 @@ class App extends Component{
   render(){
     return(
       <div className="App" id="View">
+        <div id="search">
+          <label>Subreddit: </label>
+          <input type="text" id="subName" defaultValue="cute"/>
+          <button onClick={this.changeSub}>View Images</button>
+        </div>
         <Viewer posts={this.state.imageList}/>
       </div>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 
 class Post extends Component{
 
-	test(e){
+	imageMissing(e){
 		e.target.style.display = "none";
 	}
 
 	render(){
 		return(
-			<img src={this.props.post.data.thumbnail} onError={this.test} alt={this.props.post.data.name} width="512" height="512"/>
+			<img 
+				src={this.props.post.data.url}
+				style={{marginLeft: "auto", marginRight: "auto", display:"block", paddingBottom:"10px"}}
+				onError={this.imageMissing} 
+				alt={this.props.post.data.name} 
+				width="60%" 
+				height="auto"
+			/>
 		)
 	}
 }
