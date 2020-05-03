c7ba075f3b4062e170c36f952bd15df2ef899e3a
annoor98 <annoor98@hotmail.com>
2020-05-02 19:33:23 -0600

    Updates

---
+++ precache-manifest.abb7f44e56055e6ff31470cb5a46cf86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5937109208af40246297e0d4d5ae8425",
+    "url": "/Reddit-Viewer/index.html"
+  },
+  {
+    "revision": "53db13f9a16727ac291a",
+    "url": "/Reddit-Viewer/static/js/2.80e057c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reddit-Viewer/static/js/2.80e057c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceed21c942c09ab31440",
+    "url": "/Reddit-Viewer/static/js/main.0da63a5e.chunk.js"
+  },
+  {
+    "revision": "fa02b64165157e9d251b",
+    "url": "/Reddit-Viewer/static/js/runtime-main.8badc639.js"
+  }
+]);
